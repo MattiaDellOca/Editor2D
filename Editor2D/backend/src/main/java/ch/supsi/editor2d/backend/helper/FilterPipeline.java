746b0bb7524f42721d64cc68e1ba6111361bd07a
Luca Di Bello <info@lucadibello.ch>
2022-10-08 18:06:50 +0200

    Created actual FilterPipeline, able to execute a series of operation on a same value

---
+++ Editor2D/backend/src/main/java/ch/supsi/editor2d/backend/helper/FilterPipeline.java
@@ -0,0 +1,65 @@
+package ch.supsi.editor2d.backend.helper;
+
+import ch.supsi.editor2d.backend.model.ImageWrapper;
+import ch.supsi.editor2d.backend.model.pipeline.Pipeline;
+import ch.supsi.editor2d.backend.model.pipeline.PipelineObserver;
+import ch.supsi.editor2d.backend.model.task.FilterTaskResult;
+
+public final class FilterPipeline extends Pipeline<ImageWrapper, FilterTaskResult> {
+    public FilterPipeline() {
+        super();
+    }
+
+    @Override
+    public FilterTaskResult run(ImageWrapper input) {
+        // Deep copy of input
+        ImageWrapper image = new ImageWrapper(input);
+
+        final int NTASKS = tasks.size();
+
+        // Cycle through tasks, one by one using Queue
+        for (int i = 1; !tasks.isEmpty(); i++) {
+            // Get next task
+            FilterTaskResult result = tasks.poll().execute(image);
+
+            // TODO: check if result is error
+            System.out.println("RESULT " + i + ": " + result.getResult().exampleData);
+
+            // Update progress
+            notifyObserversProgress((float) i / NTASKS);
+
+            // Save image result for next task
+            image = result.getResult();
+        }
+
+        // Notify observers that pipeline has finished
+        notifyObserversFinished();
+
+        // Return FilterTaskResult with final image
+        return new FilterTaskResult(image);
+    }
+
+    @Override
+    public void addObserver(PipelineObserver observer) {
+        this.observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(PipelineObserver observer) {
+        this.observers.add(observer);
+    }
+
+    @Override
+    public void notifyObserversFinished() {
+        for (PipelineObserver observer : observers) {
+            observer.finished();
+        }
+    }
+
+    @Override
+    public void notifyObserversProgress(float progress) {
+        for (PipelineObserver observer : observers) {
+            observer.update(progress);
+        }
+    }
+}
