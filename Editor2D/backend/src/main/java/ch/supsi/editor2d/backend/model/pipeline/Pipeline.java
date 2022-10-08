0634b06b43b1ec03e6688d7f3d62aa063e186214
Luca Di Bello <info@lucadibello.ch>
2022-10-08 18:06:22 +0200

    Removed useless abstract redefinition

--- Editor2D/backend/src/main/java/ch/supsi/editor2d/backend/model/pipeline/Pipeline.java
+++ Editor2D/backend/src/main/java/ch/supsi/editor2d/backend/model/pipeline/Pipeline.java
@@ -7,6 +7,4 @@ import java.util.List;
 
 public abstract class Pipeline<T, K extends TaskResult<?>> extends TaskExecutor<T, K> implements ObservablePipeline {
     protected final List<PipelineObserver> observers = new LinkedList<>();
-
-    public abstract K run(T input);
 }
