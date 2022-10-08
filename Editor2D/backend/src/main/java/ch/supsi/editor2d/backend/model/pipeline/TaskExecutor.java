d3ba229311fac1fbb23a0696fa44f9e2757ac88c
Luca Di Bello <info@lucadibello.ch>
2022-10-08 18:00:32 +0200

    Created TaskExecutor class to manage the pipeline queue

---
+++ Editor2D/backend/src/main/java/ch/supsi/editor2d/backend/model/pipeline/TaskExecutor.java
@@ -0,0 +1,37 @@
+package ch.supsi.editor2d.backend.model.pipeline;
+
+import ch.supsi.editor2d.backend.model.task.Task;
+import ch.supsi.editor2d.backend.model.task.TaskResult;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public abstract class TaskExecutor<T, K extends TaskResult<?>> implements Executor<T, K> {
+
+    protected final Queue<Task<T, K>> tasks = new LinkedList<>();
+
+    public abstract K run(T startInput);
+
+    @Override
+    public void clear() {
+        this.tasks.clear();
+    }
+
+    @Override
+    public void add(Task<T, K> task) {
+        this.tasks.add(task);
+    }
+
+    @Override
+    public void add(Collection<Task<T, K>> tasks) {
+        for (Task<T, K> task : tasks) {
+            this.add(task);
+        }
+    }
+
+    @Override
+    public void remove(Task<T, K> task) {
+        this.tasks.remove(task);
+    }
+}
