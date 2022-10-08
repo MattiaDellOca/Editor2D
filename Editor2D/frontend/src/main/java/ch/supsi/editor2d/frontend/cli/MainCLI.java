e068998c4f07902f03e74bad58fdd978061a3837
Luca Di Bello <info@lucadibello.ch>
2022-10-08 18:07:30 +0200

    Added test code for FilterPipeline: will be removed after co-workers update

--- Editor2D/frontend/src/main/java/ch/supsi/editor2d/frontend/cli/MainCLI.java
+++ Editor2D/frontend/src/main/java/ch/supsi/editor2d/frontend/cli/MainCLI.java
@@ -1,11 +1,46 @@
 package ch.supsi.editor2d.frontend.cli;
 
+import ch.supsi.editor2d.backend.helper.FilterPipeline;
+import ch.supsi.editor2d.backend.model.ImageWrapper;
+import ch.supsi.editor2d.backend.model.pipeline.PipelineObserver;
+import ch.supsi.editor2d.backend.model.task.FilterTask;
+import ch.supsi.editor2d.backend.model.task.FilterTaskResult;
 
-import ch.supsi.editor2d.controller.MainController;
+import java.util.Arrays;
 
-public class MainCLI {
+public class MainCLI implements PipelineObserver {
     public static void main(String[] args) {
-        MainController mainController = new MainController();
-        mainController.SayHello();
+        MainCLI mainCLI = new MainCLI();
+
+        // Image to edit
+        ImageWrapper wrapper = new ImageWrapper();
+
+        // Create FilterPipeline
+        FilterPipeline pipeline = new FilterPipeline();
+        pipeline.addObserver(mainCLI);
+
+        // add tasks to pipeline
+        FilterTask task1 = new FilterTask();
+        FilterTask task2 = new FilterTask();
+        FilterTask task3 = new FilterTask();
+        FilterTask task4 = new FilterTask();
+        pipeline.add(Arrays.asList(task1, task2, task3, task4));
+
+        // Print start value
+        System.out.println("START: " + wrapper.exampleData);
+
+        // Start pipeline
+        FilterTaskResult result = pipeline.run(wrapper);
+        System.out.println(result);
+    }
+
+    @Override
+    public void finished() {
+        System.out.println("=== OBSERVER: PIPELINE HAS FINISHED ===");
+    }
+
+    @Override
+    public void update(float progress) {
+        System.out.println("=== OBSERVER: " + (progress*100) + "%");
     }
 }
