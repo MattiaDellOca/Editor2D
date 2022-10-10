d35162037c240a6dc5d07537b12cfdd56c82084c
Luca Di Bello <info@lucadibello.ch>
2022-10-10 21:40:38 +0200

    Moved test code to JUnit test class

--- Editor2D/frontend/src/main/java/ch/supsi/editor2d/frontend/cli/MainCLI.java
+++ Editor2D/frontend/src/main/java/ch/supsi/editor2d/frontend/cli/MainCLI.java
@@ -1,36 +1,15 @@
 package ch.supsi.editor2d.frontend.cli;
 
 import ch.supsi.editor2d.backend.helper.FilterPipeline;
-import ch.supsi.editor2d.backend.model.ImageWrapper;
-import ch.supsi.editor2d.backend.model.filter.IdentityFilter;
 import ch.supsi.editor2d.backend.model.pipeline.PipelineObserver;
-import ch.supsi.editor2d.backend.model.task.FilterTask;
-import ch.supsi.editor2d.backend.model.task.FilterTaskResult;
-import javafx.scene.paint.Color;
-
-import java.util.Arrays;
 
 public class MainCLI implements PipelineObserver {
     public static void main(String[] args) {
+        // Just to showcase how to use observers until the actual test is written
         MainCLI mainCLI = new MainCLI();
-
-        // Image to edit
-        ImageWrapper wrapper = new ImageWrapper(10, 10, new Color[10][10]);
-
-        // Create FilterPipeline
         FilterPipeline pipeline = new FilterPipeline();
         pipeline.addObserver(mainCLI);
 
-        // add tasks to pipeline
-        FilterTask task1 = new FilterTask(new IdentityFilter());
-        FilterTask task2 = new FilterTask(new IdentityFilter());
-        FilterTask task3 = new FilterTask(new IdentityFilter());
-        FilterTask task4 = new FilterTask(new IdentityFilter());
-        pipeline.add(Arrays.asList(task1, task2, task3, task4));
-
-        // Start pipeline
-        FilterTaskResult result = pipeline.run(wrapper);
-        System.out.println(result);
     }
 
     @Override
