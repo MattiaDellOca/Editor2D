9ef6b99ed26574bf76201436c0ff66938819c48d
Luca Di Bello <info@lucadibello.ch>
2022-10-10 19:07:50 +0200

    Added MatrixFilter field into FilterTask

--- Editor2D/backend/src/main/java/ch/supsi/editor2d/backend/model/task/FilterTask.java
+++ Editor2D/backend/src/main/java/ch/supsi/editor2d/backend/model/task/FilterTask.java
@@ -1,14 +1,25 @@
 package ch.supsi.editor2d.backend.model.task;
 
 import ch.supsi.editor2d.backend.model.ImageWrapper;
+import ch.supsi.editor2d.backend.model.filter.MatrixFilter;
 
 public class FilterTask implements Task<ImageWrapper, FilterTaskResult> {
+
+    private final MatrixFilter filter;
+
+    public FilterTask (MatrixFilter filter) {
+        this.filter = filter;
+    }
+
     @Override
     public FilterTaskResult execute(ImageWrapper input) {
         // Read last letter of exampleData + compute next letter
-        char lastLetter = input.exampleData.charAt(input.exampleData.length() - 1);
-        char nextLetter = (char) (lastLetter + 1);
-        input.exampleData += nextLetter;
+        /*
+            char lastLetter = input.exampleData.charAt(input.exampleData.length() - 1);
+            char nextLetter = (char) (lastLetter + 1);
+            input.exampleData += nextLetter;
+            return new FilterTaskResult(input);
+         */
         return new FilterTaskResult(input);
     }
 }
