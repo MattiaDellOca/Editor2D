7640bb2b1aad8f16c044956c3bbf8fd8a7d24367
Luca Di Bello <info@lucadibello.ch>
2022-10-23 19:41:07 +0200

    Removed extra space

--- frontend/src/main/java/ch/supsi/editor2d/frontend/gui/controller/Start.java
+++ frontend/src/main/java/ch/supsi/editor2d/frontend/gui/controller/Start.java
@@ -8,7 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-
 public class Start extends Application {
     @Override
     public void start(Stage stage) throws Exception {
