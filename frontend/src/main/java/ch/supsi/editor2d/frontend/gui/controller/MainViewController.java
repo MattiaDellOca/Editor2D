dfcf78639816a4ed188043d367285fe9b44a596b
Luca Di Bello <info@lucadibello.ch>
2022-10-23 11:45:17 +0200

    Added JavaDoc

--- frontend/src/main/java/ch/supsi/editor2d/frontend/gui/controller/MainViewController.java
+++ frontend/src/main/java/ch/supsi/editor2d/frontend/gui/controller/MainViewController.java
@@ -9,19 +9,41 @@ import javafx.scene.layout.Pane;
 
 import java.io.File;
 
+/**
+ * Main view controller that handles the main view logic.
+ */
 public class MainViewController {
 
+    /**
+     * List of supported file extensions
+     */
     private final String[] SUPPORTED_FORMATS = new String[] { "ppm", "pgm", "pbm", "tga" };
 
+    /**
+     * Data model reference
+     */
     private DataModel model;
 
+    /**
+     * File drop event handler
+     */
     private EventHandler<FileDropEvent> fileDropped = event -> {};
 
+    /**
+     * About menu event handler
+     */
     private EventHandler<ActionEvent> onAboutMenuClicked = event -> {};
 
+    /**
+     * Image pane reference
+     */
     @FXML
     private Pane imagePane;
 
+    /**
+     * Initialize the model reference
+     * @param model Data model
+     */
     public void initModel(DataModel model){
 
         // ensure model is only set once
@@ -74,35 +96,66 @@ public class MainViewController {
     }
 
 
+    /**
+     * Set the file dropped event handler
+     * @param event File drop event
+     */
     public void setOnFileDropped(EventHandler<FileDropEvent> event) {
         this.fileDropped = event;
     }
 
+    /**
+     * Get the file dropped event handler
+     * @return File drop event handler
+     */
     public EventHandler<FileDropEvent> getOnFileDropped() {
         return this.fileDropped;
     }
 
+    /**
+     * Set the about menu event handler
+     * @param event About menu event
+     */
     public void setOnAboutMenuClicked(EventHandler<ActionEvent> event) {
         this.onAboutMenuClicked = event;
     }
 
+    /**
+     * Get the about menu event handler
+     * @return About menu event handler
+     */
     public EventHandler<ActionEvent> getOnAboutMenuClicked() {
         return this.onAboutMenuClicked;
     }
 
+    /**
+     * Get the image pane reference
+     * @return Image pane
+     */
     public Pane getImagePane() {
         return imagePane;
     }
 
-
+    /**
+     * Handle the zoom out action
+     * @param actionEvent Action event
+     */
     public void zoomOut(ActionEvent actionEvent) {
         System.out.println("ZOOMING OUT: " + actionEvent);
     }
 
+    /**
+     * Handle the zoom in action
+     * @param actionEvent Action event
+     */
     public void zoomIn(ActionEvent actionEvent) {
         System.out.println("ZOOMING IN: " + actionEvent);
     }
 
+    /**
+     * Handle the about menu action
+     * @param actionEvent Action event
+     */
     public void onAboutMenu(ActionEvent actionEvent) {
         // Run related event handler
         getOnAboutMenuClicked().handle(actionEvent);
