6fd3305d48aca8656f2422027c49f04429b34ae1
Luca Di Bello <info@lucadibello.ch>
2022-10-31 15:49:33 +0100

    Now has a EventHandler, needed to notify main when to refresh the image container

--- frontend/src/main/java/ch/supsi/editor2d/frontend/gui/controller/PipelineCellViewController.java
+++ frontend/src/main/java/ch/supsi/editor2d/frontend/gui/controller/PipelineCellViewController.java
@@ -1,9 +1,13 @@
 package ch.supsi.editor2d.frontend.gui.controller;
 
+import ch.supsi.editor2d.backend.exception.PipelineException;
 import ch.supsi.editor2d.backend.model.ImageWrapper;
 import ch.supsi.editor2d.backend.model.task.FilterTaskResult;
 import ch.supsi.editor2d.backend.model.task.Task;
+import ch.supsi.editor2d.frontend.gui.alert.ErrorAlert;
 import ch.supsi.editor2d.frontend.gui.model.DataModel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
@@ -19,6 +23,7 @@ public class PipelineCellViewController {
 
     private Task<ImageWrapper, FilterTaskResult> task;
 
+    private EventHandler<ActionEvent> onFilterRemovedSuccessfully = event -> {};
 
     public void initModel(DataModel model) {
         // ensure model is only set once
@@ -36,8 +41,20 @@ public class PipelineCellViewController {
         this.task = task;
     }
 
+    public void setOnFilterRemovedSuccessfully(EventHandler<ActionEvent> onFilterRemovedSuccessfully) {
+        this.onFilterRemovedSuccessfully = onFilterRemovedSuccessfully;
+    }
+
     @FXML
     public void removeHandler() {
-        model.removeTaskFromPipeline(task);
+        try {
+            // Remove task from pipeline + rerun pipeline
+            model.removeTaskFromPipeline(task);
+            // Notify that image has been updated
+            onFilterRemovedSuccessfully.handle(new ActionEvent());
+        } catch (PipelineException e) {
+            System.err.println(e.getMessage());
+            ErrorAlert.showError(e.getMessage());
+        }
     }
 }
