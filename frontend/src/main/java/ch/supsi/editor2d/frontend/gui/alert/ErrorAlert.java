f7f8dc1026992e4e49e81d411674bfc04be7da77
Luca Di Bello <info@lucadibello.ch>
2022-12-30 18:21:05 +0100

    Now adding also some IDs

--- frontend/src/main/java/ch/supsi/editor2d/frontend/gui/alert/ErrorAlert.java
+++ frontend/src/main/java/ch/supsi/editor2d/frontend/gui/alert/ErrorAlert.java
@@ -4,11 +4,15 @@ import javafx.scene.control.Alert;
 
 public class ErrorAlert {
     public static void showError(String message){
+        // Create the dialog
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error Dialog");
         alert.setHeaderText(message);
         alert.setContentText(":(");
-        System.err.println(message);
+        // Add IDs to be able to test the dialog
+        alert.getDialogPane().setId("errorDialog");
+        alert.getDialogPane().lookupButton(alert.getButtonTypes().get(0)).setId("errorDialogOkButton");
+        // Show the dialog
         alert.showAndWait();
     }
 }
